68a0fb813a835e89a2b3d36c598fd7e84ebe741b
Aleksa Camilovic <aleksa.camilovic@quantox.com>
2022-09-05 18:03:07 +0200

    Config - Vite - Add hot reload config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost'
+    },
     watch: {
       usePolling: true,
     },
