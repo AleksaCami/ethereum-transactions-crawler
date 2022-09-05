d2de9ed79a50e427f45c69ecfc4e9941ee581773
Aleksa Camilovic <aleksa.camilovic@quantox.com>
2022-09-05 18:03:44 +0200

    Utils - General - Add formatTimestampToDate util

--- src/utils/general.utils.js
+++ src/utils/general.utils.js
@@ -2,7 +2,7 @@
  * Checks is value empty, null or undefined.
  * If value is array, checks if the length of the array is greater than zero.
  * @param value - Value to be checked.
- * @returns Is value empty boolean.
+ * @returns {boolean} Is value empty boolean.
  */
 export const isEmpty = (value) => {
   if (
@@ -21,3 +21,14 @@ export const isEmpty = (value) => {
 
   return false;
 };
+
+/**
+ * Formats timestamp to ISO string date.
+ * @param {number} timestamp 
+ * @returns {Date}
+ */
+export const formatTimestampToDate = (timestamp) => {
+  const date = new Date(timestamp * 1000).toISOString();
+
+  return date;
+}
